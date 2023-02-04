07d6fd1e590b04ee98c42fc3fef654712d8e260c
jwhong <jwhong98@gmail.com>
2023-02-04 11:18:40 -0800

    added base SelectPlan component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import FormNav from "./components/FormNav/FormNav";
 import PersonalInfo from "./components/PersonalInfo/PersonalInfo";
+import SelectPlan from "./components/SelectPlan/SelectPlan";
 import ThankYou from "./components/ThankYou/ThankYou";
 
 function App() {
@@ -8,7 +9,8 @@ function App() {
     <div className="App">
       <FormNav />
       <div className="container">
-        <PersonalInfo />
+        {/* <PersonalInfo /> */}
+        <SelectPlan />
         {/* <ThankYou /> */}
       </div>
     </div>
--- src/components/SelectPlan/SelectPlan.js
+++ src/components/SelectPlan/SelectPlan.js
@@ -2,7 +2,18 @@ import React from "react";
 import classes from "./SelectPlan.module.css";
 
 const SelectPlan = () => {
-  return <div>SelectPlan</div>;
+  return (
+    <section className={classes.selectPlan}>
+      <div className={classes.selectPlan__header}>
+        <h1>Select your plan</h1>
+        <p>You have the option of monthly or yearly billing.</p>
+      </div>
+      <div className={classes.selectPlan__plans}></div>
+      <div className={classes.selectPlan__switch}>
+        <span>Monthly Yearly</span>
+      </div>
+    </section>
+  );
 };
 
 export default SelectPlan;
