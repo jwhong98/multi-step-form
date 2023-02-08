dab0db7cfd6b0ed97dd789c41f31d181d97557a9
jwhong <jwhong98@gmail.com>
2023-02-08 12:53:09 -0800

    added routing with FormNav and components

--- src/App.js
+++ src/App.js
@@ -5,18 +5,23 @@ import PersonalInfo from "./components/PersonalInfo/PersonalInfo";
 import SelectPlan from "./components/SelectPlan/SelectPlan";
 import Summary from "./components/Summary/Summary";
 import ThankYou from "./components/ThankYou/ThankYou";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <FormNav />
-      <div className="container">
-        {/* <PersonalInfo /> */}
-        {/* <SelectPlan /> */}
-        {/* <AddOns /> */}
-        <Summary />
-        {/* <ThankYou /> */}
-      </div>
+      <BrowserRouter>
+        <FormNav />
+        <div className="container">
+          <Routes>
+            <Route index element={<PersonalInfo />} />
+            <Route path="/selectPlan" element={<SelectPlan />} />
+            <Route path="/addOns" element={<AddOns />} />
+            <Route path="/summary" element={<Summary />} />
+            {/* <ThankYou /> */}
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/FormNav/FormNav.js
+++ src/components/FormNav/FormNav.js
@@ -1,38 +1,84 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from "./FormNav.module.css";
+import { Outlet, Link } from "react-router-dom";
 
-const FormNav = (props) => {
+const FormNav = () => {
+  const [active, setActive] = useState("1");
+
+  const onClickHandler = (e) => {
+    console.log(e.target.dataset.id);
+    setActive(e.target.dataset.id);
+  };
   return (
-    <aside className={classes.nav}>
-      <div className={classes.nav__tab}>
-        <div className={`${classes.badge} ${classes.active}`}>1</div>
-        <div className={classes.content}>
-          <p>step 1</p>
-          <p className={classes.title}>your info</p>
-        </div>
-      </div>
-      <div className={classes.nav__tab}>
-        <div className={classes.badge}>2</div>
-        <div className={classes.content}>
-          <p>step 2</p>
-          <p className={classes.title}>select plan</p>
-        </div>
-      </div>
-      <div className={classes.nav__tab}>
-        <div className={classes.badge}>3</div>
-        <div className={classes.content}>
-          <p>step 3</p>
-          <p className={classes.title}>add-ons</p>
-        </div>
-      </div>
-      <div className={classes.nav__tab}>
-        <div className={classes.badge}>4</div>
-        <div className={classes.content}>
-          <p>step 4</p>
-          <p className={classes.title}>summary</p>
-        </div>
-      </div>
-    </aside>
+    <>
+      <aside className={classes.nav}>
+        <Link
+          to="/"
+          className={classes.nav__tab}
+          data-id="1"
+          onClick={onClickHandler}
+        >
+          <div
+            className={`${classes.badge} ${active === "1" && classes.active}`}
+          >
+            1
+          </div>
+          <div className={classes.content}>
+            <p>step 1</p>
+            <p className={classes.title}>your info</p>
+          </div>
+        </Link>
+        <Link
+          to="/selectPlan"
+          className={classes.nav__tab}
+          data-id="2"
+          onClick={onClickHandler}
+        >
+          <div
+            className={`${classes.badge} ${active === "2" && classes.active}`}
+          >
+            2
+          </div>
+          <div className={classes.content}>
+            <p>step 2</p>
+            <p className={classes.title}>select plan</p>
+          </div>
+        </Link>
+        <Link
+          to="/addOns"
+          className={classes.nav__tab}
+          data-id="3"
+          onClick={onClickHandler}
+        >
+          <div
+            className={`${classes.badge} ${active === "3" && classes.active}`}
+          >
+            3
+          </div>
+          <div className={classes.content}>
+            <p>step 3</p>
+            <p className={classes.title}>add-ons</p>
+          </div>
+        </Link>
+        <Link
+          to="/summary"
+          className={classes.nav__tab}
+          data-id="4"
+          onClick={onClickHandler}
+        >
+          <div
+            className={`${classes.badge} ${active === "4" && classes.active}`}
+          >
+            4
+          </div>
+          <div className={classes.content}>
+            <p>step 4</p>
+            <p className={classes.title}>summary</p>
+          </div>
+        </Link>
+      </aside>
+      <Outlet />
+    </>
   );
 };
 
