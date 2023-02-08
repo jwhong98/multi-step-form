60a9d03b7991a2e6661c6c80098fd7954b1fa5bc
jwhong <jwhong98@gmail.com>
2023-02-08 13:41:26 -0800

    changed nav tab active state

--- src/components/FormNav/FormNav.js
+++ src/components/FormNav/FormNav.js
@@ -1,25 +1,18 @@
-import React, { useState } from "react";
+import React from "react";
 import classes from "./FormNav.module.css";
-import { Outlet, Link } from "react-router-dom";
+import { Outlet, Link, useLocation } from "react-router-dom";
 
 const FormNav = () => {
-  const [active, setActive] = useState("1");
+  const location = useLocation();
 
-  const onClickHandler = (e) => {
-    console.log(e.target.dataset.id);
-    setActive(e.target.dataset.id);
-  };
   return (
     <>
       <aside className={classes.nav}>
-        <Link
-          to="/"
-          className={classes.nav__tab}
-          data-id="1"
-          onClick={onClickHandler}
-        >
+        <Link to="/" className={classes.nav__tab} data-id="1">
           <div
-            className={`${classes.badge} ${active === "1" && classes.active}`}
+            className={`${classes.badge} ${
+              location.pathname === "/" && classes.active
+            }`}
           >
             1
           </div>
@@ -28,14 +21,11 @@ const FormNav = () => {
             <p className={classes.title}>your info</p>
           </div>
         </Link>
-        <Link
-          to="/selectPlan"
-          className={classes.nav__tab}
-          data-id="2"
-          onClick={onClickHandler}
-        >
+        <Link to="/selectPlan" className={classes.nav__tab}>
           <div
-            className={`${classes.badge} ${active === "2" && classes.active}`}
+            className={`${classes.badge} ${
+              location.pathname === "/selectPlan" && classes.active
+            }`}
           >
             2
           </div>
@@ -44,14 +34,11 @@ const FormNav = () => {
             <p className={classes.title}>select plan</p>
           </div>
         </Link>
-        <Link
-          to="/addOns"
-          className={classes.nav__tab}
-          data-id="3"
-          onClick={onClickHandler}
-        >
+        <Link to="/addOns" className={classes.nav__tab}>
           <div
-            className={`${classes.badge} ${active === "3" && classes.active}`}
+            className={`${classes.badge} ${
+              location.pathname === "/addOns" && classes.active
+            }`}
           >
             3
           </div>
@@ -60,14 +47,11 @@ const FormNav = () => {
             <p className={classes.title}>add-ons</p>
           </div>
         </Link>
-        <Link
-          to="/summary"
-          className={classes.nav__tab}
-          data-id="4"
-          onClick={onClickHandler}
-        >
+        <Link to="/summary" className={classes.nav__tab}>
           <div
-            className={`${classes.badge} ${active === "4" && classes.active}`}
+            className={`${classes.badge} ${
+              location.pathname === "/summary" && classes.active
+            }`}
           >
             4
           </div>
